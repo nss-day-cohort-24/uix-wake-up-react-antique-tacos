55a3ed984f0138779a7b32ac4aeea7732a9ddc78
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-04-18 10:28:54 -0500

    created class w/super(props) and api call

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,1 +1,31 @@
 import React, { Component } from 'react';
+
+
+class Quote extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            newsLoaded: false,
+            objResult: {},
+            showResult: false,
+        }
+    }
+    componentDidMount() {
+        this.getQuote()
+    }
+    getQuote(){
+        fetch(`http://quotes.rest/qod.json?category=inspire`)
+        .then(data => data.json())
+        .then(
+            (result) => {
+                console.log("Quote: ", result);
+                this.setState({
+                    quoteLoaded = true,
+                    objResult = result
+                });
+            },
+        )
+    }
+
+}
